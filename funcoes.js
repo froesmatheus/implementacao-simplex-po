30083bd8a4e459ed62ab43354c5bcdf20a58db42
Matheus Fróes Marques <matheusfroesmarques@gmail.com>
2017-03-22 15:20:40 -0300

    js modificado

--- funcoes.js
+++ funcoes.js
@@ -1,252 +1,266 @@
 function condicaoParada(p_matriz) {
-	var i = p_matriz.length - 1;
+		var i = p_matriz.length - 1;
 
-	for (j = 1; j < p_matriz[i].length; j++) {
-		if (p_matriz[i][j] > 0) {
-			return true;
+		for (j = 1; j < p_matriz[i].length; j++) {
+			if (p_matriz[i][j] > 0) {
+				return true;
+			}
 		}
-	}
-	return false;
-}
-
-function calcMatriz(p_matriz) {
-	var nLinhas = p_matriz.length - 1;
-	var nColunas = p_matriz[nLinhas].length - 1;
-	var maior = p_matriz[nLinhas][1];
-	indMaior = 1;
-	for (j = 2; j <= nColunas; j++) {
-		if (p_matriz[nLinhas][j] > maior) {
-			maior = p_matriz[nLinhas][j];
-			indMaior = j;
-		}
+		return false;
 	}
 
-	var menor = Number.MAX_VALUE;
-	var indMenor = 0;
-	for (k = 1; k < nLinhas; k++) {
-		if (p_matriz[k][indMaior] != 0 && p_matriz[k][nColunas] / p_matriz[k][indMaior] < menor) {
-			menor = p_matriz[k][nColunas] / p_matriz[k][indMaior];
-			indMenor = k;
+	function calcMatriz(p_matriz) {
+		var nLinhas = p_matriz.length - 1;
+		var nColunas = p_matriz[nLinhas].length - 1;
+		var maior = p_matriz[nLinhas][1];
+		indMaior = 1;
+		for (j = 2; j <= nColunas; j++) {
+			if (p_matriz[nLinhas][j] > maior) {
+				maior = p_matriz[nLinhas][j];
+				indMaior = j;
+			}
 		}
-	}
 
-	p_matriz[indMenor][0] = p_matriz[0][indMaior];
+		var menor = Number.MAX_VALUE;
+		var indMenor = 0;
+		for (k = 1; k < nLinhas; k++) {
+			if (p_matriz[k][indMaior] != 0 && p_matriz[k][nColunas] / p_matriz[k][indMaior] < menor) {
+				menor = p_matriz[k][nColunas] / p_matriz[k][indMaior];
+				indMenor = k;
+			}
+		}
 	
-	var aux = p_matriz[indMenor][indMaior];
-	for (l = 1; l <= nColunas; l++) {
-		p_matriz[indMenor][l] = p_matriz[indMenor][l] / aux;
-	}
-}
-
-function esconder(p_variaveis, p_restricoes) {
-	for (i = 1; i <= p_variaveis; i++) {
-		//document.getElementById('y'+i).style.outline = "0";
-		document.getElementById('y'+i).style.border = "0";
-		document.getElementById('y'+i).readOnly = true;
-		for (j = 1; j <= p_restricoes; j++) {
-			//document.getElementById('x'+i+j).style.outline = "0";
-			document.getElementById('x'+i+j).style.border = "0";
-			document.getElementById('x'+i+j).readOnly = true;
+		p_matriz[indMenor][0] = p_matriz[0][indMaior];
+		
+		printTabela(p_matriz);
+		
+		var aux = p_matriz[indMenor][indMaior];
+		for (l = 1; l <= nColunas; l++) {
+			p_matriz[indMenor][l] = p_matriz[indMenor][l] / aux;
+		}
+		
+		printTabela(p_matriz);
+		
+		for (i = 1; i <= nLinhas; i++) {
+			var aux = p_matriz[i][indMaior];
+			if (i != indMenor) {
+				for (j = 1; j <= nColunas; j++) {
+					p_matriz[i][j] = parseInt(p_matriz[i][j]) + parseInt(-1 * aux * p_matriz[indMenor][j]);
+				}
+				printTabela(p_matriz);
+			}
 		}
 	}
-	for (j = 1; j <= p_restricoes; j++) {
-		//document.getElementById('b'+j).style.outline = "0";
-		document.getElementById('b'+j).style.border = "0";
-		document.getElementById('b'+j).readOnly = true;
-	}
-}
 
-function validarCoeficientes(p_variaveis, p_restricoes) {
-	for (i = 1; i <= p_variaveis; i++) {
-		if (document.getElementById('y'+i).value == "") {
-			document.getElementById('y'+i).focus();
-			alert('Informe os valores de todos os coeficientes.');
-			return 1;
+	function esconder(p_variaveis, p_restricoes) {
+		for (i = 1; i <= p_variaveis; i++) {
+			//document.getElementById('y'+i).style.outline = "0";
+			document.getElementById('y'+i).style.border = "0";
+			document.getElementById('y'+i).readOnly = true;
+			for (j = 1; j <= p_restricoes; j++) {
+				//document.getElementById('x'+i+j).style.outline = "0";
+				document.getElementById('x'+i+j).style.border = "0";
+				document.getElementById('x'+i+j).readOnly = true;
+			}
 		}
 		for (j = 1; j <= p_restricoes; j++) {
-			if (document.getElementById('x'+i+j).value == "") {
-				document.getElementById('x'+i+j).focus();
+			//document.getElementById('b'+j).style.outline = "0";
+			document.getElementById('b'+j).style.border = "0";
+			document.getElementById('b'+j).readOnly = true;
+		}
+	}
+
+	function validarCoeficientes(p_variaveis, p_restricoes) {
+		for (i = 1; i <= p_variaveis; i++) {
+			if (document.getElementById('y'+i).value == "") {
+				document.getElementById('y'+i).focus();
 				alert('Informe os valores de todos os coeficientes.');
+				return 1;
+			}
+			for (j = 1; j <= p_restricoes; j++) {
+				if (document.getElementById('x'+i+j).value == "") {
+					document.getElementById('x'+i+j).focus();
+					alert('Informe os valores de todos os coeficientes.');
+					return 1;
+				}
+			}
+		}
+		for (j = 1; j <= p_restricoes; j++) {
+			if (document.getElementById('b'+j).value == "") {
+				document.getElementById('b'+j).focus();
+				alert('Informe os valores de todas as constantes.');
 				return 1;
 			}
 		}
 	}
-	for (j = 1; j <= p_restricoes; j++) {
-		if (document.getElementById('b'+j).value == "") {
-			document.getElementById('b'+j).focus();
-			alert('Informe os valores de todas as constantes.');
-			return 1;
-		}
-	}
-}
 
-function atualizar() {
-	window.location.href='simplex.html';
-}
-
-function criarForm(p_variaveis, p_restricoes) {
-	
-	if (p_variaveis == "" || p_variaveis <= 0) {
-		alert('Preencha o campo com a quantidade de variáveis.');
-		form1.variaveis.focus();
-	} else {
-		if (p_restricoes == "" || p_restricoes <= 0) {
-			alert('Preencha o campo com a quantidade de restrições.');
-			form1.regras.focus();
-		}
+	function atualizar() {
+		window.location.href='simplex.html';
 	}
-	if (p_variaveis > 0 && p_restricoes > 0) {
-		document.getElementById("form2").style.display = 'block';
-		document.getElementById("aqui").innerHTML+="<label>Z = </label>";
-		document.getElementById("aqui").innerHTML+="<input type='number' class='inputZ' required size='5' maxlength='10' step='0.1' id='y1' name='y1' />x<sub>1</sub>";
-		for (var h = 2; h <= p_variaveis; h++) {
-			document.getElementById("aqui").innerHTML+=" + <input type='number' class='inputZ' required size='5' maxlength='10' step='0.1' id='y"+h+"' name='y"+h+"' />x<sub>"+h+"</sub>";
-		}
-		for (var i = 1; i <= p_restricoes; i++) {
-			document.getElementById("aqui").innerHTML+="<p><b>Restrição "+i+"</b></p>";
-			document.getElementById("aqui").innerHTML+="<input type='number' class='input' required size='5' maxlength='10' step='0.1' id='x"+i+"1' name='x"+i+"1' />x<sub>1</sub>";
-			for (var j = 2; j <= p_variaveis; j++) {
-				document.getElementById("aqui").innerHTML+=" + <input type='number' class='input' required size='5' maxlength='10' step='0.1' id='x"+i+j+"' name='x"+i+j+"' />x<sub>"+j+"</sub>";
-			}
-			document.getElementById("aqui").innerHTML+="<label> <= </label>"
-			+"<input type='number' class='input' required size='5' maxlength='10' id='b"+i+"' name='b"+i+"' style='text-align:left' />";
-		}
-		document.getElementById("aqui").innerHTML+="<p><b>Restrição "+(++p_restricoes)+"</b></p>"
-		+"<p>x<sub>i</sub> >= 0</p>";
-		document.getElementById("btn1").style.display = 'none';
-		document.getElementById("in1").disabled = true;
-		document.getElementById("in2").disabled = true;
+
+	function criarForm(p_variaveis, p_restricoes) {
 		
-	}
-} 
-
-function printTabela(p_matriz) {
-	var restricoes = parseInt(document.form1.regras.value);
-	var variaveis = parseInt(document.form1.variaveis.value);
-	var linhas = restricoes+1;
-	var colunas = restricoes + variaveis+1;
-	var tabela = document.createElement("table");
-	var thead = document.createElement("thead");
-	var tbody=document.createElement("tbody");
-  
-	var tr = document.createElement("tr");
-	for (var l = 0; l <= colunas; l++) {
-		var variavel = p_matriz[0][l];
-		var th = document.createElement("th");
-		if(l == 0) {
-			var texto = document.createTextNode(variavel);
-			th.appendChild(texto)
+		if (p_variaveis == "" || p_variaveis <= 0) {
+			alert('Preencha o campo com a quantidade de variáveis.');
+			form1.variaveis.focus();
 		} else {
-			var sub = document.createElement("sub");
-			var textoSub = document.createTextNode(variavel.substr(1,1));
-			var texto = document.createTextNode(variavel.substr(0,1));
-			sub.appendChild(textoSub)
-			th.appendChild(sub);
-			th.insertBefore(texto, th.firstChild);
+			if (p_restricoes == "" || p_restricoes <= 0) {
+				alert('Preencha o campo com a quantidade de restrições.');
+				form1.regras.focus();
+			}
 		}
-		tr.appendChild(th);
-	}
-	thead.appendChild(tr);
-	
-	for(var n = 1; n <= linhas; n++) {
+		if (p_variaveis > 0 && p_restricoes > 0) {
+			document.getElementById("form2").style.display = 'block';
+			document.getElementById("aqui").innerHTML+="<label>Z = </label>";
+			document.getElementById("aqui").innerHTML+="<input type='number' class='inputZ' required size='5' maxlength='10' step='0.1' id='y1' name='y1' />x<sub>1</sub>";
+			for (var h = 2; h <= p_variaveis; h++) {
+				document.getElementById("aqui").innerHTML+=" + <input type='number' class='inputZ' required size='5' maxlength='10' step='0.1' id='y"+h+"' name='y"+h+"' />x<sub>"+h+"</sub>";
+			}
+			for (var i = 1; i <= p_restricoes; i++) {
+				document.getElementById("aqui").innerHTML+="<p><b>Restrição "+i+"</b></p>";
+				document.getElementById("aqui").innerHTML+="<input type='number' class='input' required size='5' maxlength='10' step='0.1' id='x"+i+"1' name='x"+i+"1' />x<sub>1</sub>";
+				for (var j = 2; j <= p_variaveis; j++) {
+					document.getElementById("aqui").innerHTML+=" + <input type='number' class='input' required size='5' maxlength='10' step='0.1' id='x"+i+j+"' name='x"+i+j+"' />x<sub>"+j+"</sub>";
+				}
+				document.getElementById("aqui").innerHTML+="<label> <= </label>"
+				+"<input type='number' class='input' required size='5' maxlength='10' id='b"+i+"' name='b"+i+"' style='text-align:left' />";
+			}
+			document.getElementById("aqui").innerHTML+="<p><b>Restrição "+(++p_restricoes)+"</b></p>"
+			+"<p>x<sub>i</sub> >= 0</p>";
+			document.getElementById("btn1").style.display = 'none';
+			document.getElementById("in1").disabled = true;
+			document.getElementById("in2").disabled = true;
+			
+		}
+	} 
+
+	function printTabela(p_matriz) {
+		var restricoes = parseInt(document.form1.regras.value);
+		var variaveis = parseInt(document.form1.variaveis.value);
+		var linhas = restricoes+1;
+		var colunas = restricoes + variaveis+1;
+		var tabela = document.createElement("table");
+		var thead = document.createElement("thead");
+		var tbody=document.createElement("tbody");
+	  
 		var tr = document.createElement("tr");
-		for(var o = 0; o <= colunas; o++) {
-			var variavel = p_matriz[n][o];
-			var td = document.createElement("td");
-			if (o == 0 && n < linhas) {
+		for (var l = 0; l <= colunas; l++) {
+			var variavel = p_matriz[0][l];
+			var th = document.createElement("th");
+			if(l == 0) {
+				var texto = document.createTextNode(variavel);
+				th.appendChild(texto)
+			} else {
 				var sub = document.createElement("sub");
 				var textoSub = document.createTextNode(variavel.substr(1,1));
 				var texto = document.createTextNode(variavel.substr(0,1));
 				sub.appendChild(textoSub)
-				td.appendChild(sub);
-				td.insertBefore(texto, td.firstChild);
-			} else {
-				var texto = document.createTextNode(variavel);
-				td.appendChild(texto);
+				th.appendChild(sub);
+				th.insertBefore(texto, th.firstChild);
 			}
-			tr.appendChild(td);
+			tr.appendChild(th);
 		}
-		tbody.appendChild(tr);
+		thead.appendChild(tr);
+		
+		for(var n = 1; n <= linhas; n++) {
+			var tr = document.createElement("tr");
+			for(var o = 0; o <= colunas; o++) {
+				var variavel = p_matriz[n][o];
+				var td = document.createElement("td");
+				if (o == 0 && n < linhas) {
+					var sub = document.createElement("sub");
+					var textoSub = document.createTextNode(variavel.substr(1,1));
+					var texto = document.createTextNode(variavel.substr(0,1));
+					sub.appendChild(textoSub)
+					td.appendChild(sub);
+					td.insertBefore(texto, td.firstChild);
+				} else {
+					var texto = document.createTextNode(variavel);
+					td.appendChild(texto);
+				}
+				tr.appendChild(td);
+			}
+			tbody.appendChild(tr);
+		}
+
+		tabela.appendChild(thead);
+		tabela.appendChild(tbody);
+		tabela.border = 1;
+		document.getElementById("tab").appendChild(tabela);
 	}
 
-	tabela.appendChild(thead);
-	tabela.appendChild(tbody);
-	tabela.border = 1;
-	document.getElementById("tab").appendChild(tabela);
-}
-
-function resolver() {
-	var restricoes = parseInt(document.form1.regras.value);
-	var variaveis = parseInt(document.form1.variaveis.value);	
-	var linhas = parseInt(document.form1.regras.value) + 1;
-	var colunas = parseInt(document.form1.variaveis.value) + parseInt(document.form1.regras.value) + 1;
-	
-	if (validarCoeficientes(variaveis, restricoes) == 1) {
-		return;
-	}
-	esconder(variaveis, restricoes);
-	
-	document.getElementById("btn2").style.display = 'none';
-	document.getElementById("btn3").style.display = 'none';
-	document.getElementById("tab").innerHTML+="<h2>Resolução</h2>";
-	document.getElementById("tab").innerHTML+="<hr/>";
-	matriz = [[]];
-	matriz[0][0] = 'VB';
-	
-	var indice = 1;
-	for (var l = 1; l <= variaveis; l++) {
-		matriz[0][indice] = "x"+indice;
-		indice++;
-	}
-	for (var m = 1; m <= restricoes; m++) {
-		matriz[0][indice] = "f"+m;
-		indice++;
-	}
-	
-	matriz[0][matriz[0].length] = 'b';
-
-	var x = document.querySelectorAll(".input");
-	indice = 0;
-	var coluna = 0;
-	for (var i = 1; i < linhas; i++) {
-		matriz.push(['f'+i]);
-	   	for (var j = 1; j <= variaveis; j++) {
-			matriz[i][j] = x[indice].value;
+	function resolver() {
+		var restricoes = parseInt(document.form1.regras.value);
+		var variaveis = parseInt(document.form1.variaveis.value);	
+		var linhas = parseInt(document.form1.regras.value) + 1;
+		var colunas = parseInt(document.form1.variaveis.value) + parseInt(document.form1.regras.value) + 1;
+		
+		if (validarCoeficientes(variaveis, restricoes) == 1) {
+			return;
+		}
+		esconder(variaveis, restricoes);
+		
+		document.getElementById("btn2").style.display = 'none';
+		document.getElementById("btn3").style.display = 'none';
+		document.getElementById("tab").innerHTML+="<h2>Resolução</h2>";
+		document.getElementById("tab").innerHTML+="<hr/>";
+		matriz = [[]];
+		matriz[0][0] = 'VB';
+		
+		var indice = 1;
+		for (var l = 1; l <= variaveis; l++) {
+			matriz[0][indice] = "x"+indice;
 			indice++;
 		}
-		coluna = variaveis + 1;
-		for (var k = 1; k <= restricoes; k++) {
-			if(i==k) {
-				matriz[i][coluna] = 1;
-			} else {
-				matriz[i][coluna] = 0;
+		for (var m = 1; m <= restricoes; m++) {
+			matriz[0][indice] = "f"+m;
+			indice++;
+		}
+		
+		matriz[0][matriz[0].length] = 'b';
+
+		var x = document.querySelectorAll(".input");
+		indice = 0;
+		var coluna = 0;
+		for (var i = 1; i < linhas; i++) {
+			matriz.push(['f'+i]);
+		   	for (var j = 1; j <= variaveis; j++) {
+				matriz[i][j] = x[indice].value;
+				indice++;
 			}
+			coluna = variaveis + 1;
+			for (var k = 1; k <= restricoes; k++) {
+				if(i==k) {
+					matriz[i][coluna] = 1;
+				} else {
+					matriz[i][coluna] = 0;
+				}
+				coluna++;
+			}
+			matriz[i][coluna] = x[indice].value;
+			indice++;
+		}
+		
+		var z = document.querySelectorAll(".inputZ");
+		coluna = 0;
+		matriz.push(['-Z']);
+		for (var l = 0; l < variaveis; l++) {
+			matriz[linhas][l+1] = z[l].value;
+		}
+		coluna = variaveis + 1;
+		for (var m = 1; m <= restricoes; m++) {
+			matriz[linhas][coluna] = 0;
 			coluna++;
 		}
-		matriz[i][coluna] = x[indice].value;
-		indice++;
-	}
-	
-	var z = document.querySelectorAll(".inputZ");
-	coluna = 0;
-	matriz.push(['-Z']);
-	for (var l = 0; l < variaveis; l++) {
-		matriz[linhas][l+1] = z[l].value;
-	}
-	coluna = variaveis + 1;
-	for (var m = 1; m <= restricoes; m++) {
 		matriz[linhas][coluna] = 0;
-		coluna++;
-	}
-	matriz[linhas][coluna] = 0;
-	
-	printTabela(matriz);
-	
-	var ite = 1;
-	while (/*condicaoParada(matriz)*/ite<5) { //ite<5 (teste)
-		calcMatriz(matriz);
-		document.getElementById("tab").innerHTML+="<p><b>Iteração "+ite+"<b/><p/>";
+		
 		printTabela(matriz);
-		ite++;
+		
+		var ite = 1;
+		while (condicaoParada(matriz)) { //ite<5 (teste)
+			calcMatriz(matriz);
+			document.getElementById("tab").innerHTML+="<p><b>Iteração "+ite+"<b/><p/>";
+			//printTabela(matriz);
+			ite++;
+		}
 	}
-}
